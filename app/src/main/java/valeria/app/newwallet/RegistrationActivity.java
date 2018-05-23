7793e87cfe61c379b9c00cef7efbdc36c0ff8e7c
ValeriaChub <v.chub@tecsynt.com>
2018-05-23 11:19:09 +0300

    Added activities

---
+++ app/src/main/java/valeria/app/newwallet/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package valeria.app.newwallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_registration);
+    }
+}
---
+++ app/src/main/java/valeria/app/newwallet/StartActivity.java
@@ -0,0 +1,13 @@
+package valeria.app.newwallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_start);
+    }
+}
